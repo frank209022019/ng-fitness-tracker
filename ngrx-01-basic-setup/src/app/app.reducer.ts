8a7db477be920d61e72a66df47fde21fc4a978cf
frankdgcapital <96611469+frankdgcapital@users.noreply.github.com>
2022-08-10 07:55:32 +0200

    Update app.reducer.ts

--- ngrx-01-basic-setup/src/app/app.reducer.ts
+++ ngrx-01-basic-setup/src/app/app.reducer.ts
@@ -12,6 +12,6 @@ export const REDUCERS: ActionReducerMap<State> = {
 }
 
 //  Access a reducer by string
-export const GET_UI_STATE = createFeatureSelector<fromUi.State>('ui');
 //  Get loader state => () : pass a param to trigger the avove GET_UI_STATE
+export const GET_UI_STATE = createFeatureSelector<fromUi.State>('ui');
 export const GET_IS_LOADING = createSelector(GET_UI_STATE, fromUi.GET_IS_LOADING);
