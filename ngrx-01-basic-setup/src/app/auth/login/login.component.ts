dfd3964814df802ca03969bace46ae64a87bcfc0
frankdgcapital <96611469+frankdgcapital@users.noreply.github.com>
2022-08-11 15:32:28 +0200

    added to signup and new training

--- ngrx-01-basic-setup/src/app/auth/login/login.component.ts
+++ ngrx-01-basic-setup/src/app/auth/login/login.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Subscription, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
-
 import { AuthService } from '../auth.service';
-import { UIService } from '../../shared/ui.service';
 import * as fromRoot from '../../app.reducer';
 
 @Component({
@@ -16,21 +13,14 @@ import * as fromRoot from '../../app.reducer';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   isLoading$: Observable<boolean>;
-  private loadingSubs: Subscription;
 
   constructor(
     private authService: AuthService,
-    private uiService: UIService,
     private store: Store<{ ui: fromRoot.State }>
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.isLoading$ = this.store.select(fromRoot.GET_IS_LOADING);
-    // this.loadingSubs = this.uiService.loadingStateChanged.subscribe(
-    //   isLoading => {
-    //     this.isLoading = isLoading;
-    //   }
-    // );
     this.loginForm = new FormGroup({
       email: new FormControl('', {
         validators: [Validators.required, Validators.email]
@@ -45,10 +35,4 @@ export class LoginComponent implements OnInit {
       password: this.loginForm.value.password
     });
   }
-
-  // ngOnDestroy() {
-  //   if (this.loadingSubs) {
-  //     this.loadingSubs.unsubscribe();
-  //   }
-  // }
 }
